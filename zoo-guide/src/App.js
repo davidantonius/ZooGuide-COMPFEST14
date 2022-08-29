8d2db861058fb7ae7054cbe975e216d331fc6639
Nathan24zz <nathanterbaik@gmail.com>
2022-08-30 02:36:42 +0700

    feat: add text input

--- zoo-guide/src/App.js
+++ zoo-guide/src/App.js
@@ -1,33 +1,41 @@
-import { Component } from 'react';
-import './App.css';
-
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   state = {
-    count: 0
+    count: 0,
   };
 
   handleIncrement = () => {
-    this.setState({count: this.state.count + 1});
-    console.log(this.state.count)
-  }
+    this.setState({ count: this.state.count + 1 });
+    console.log(this.state.count);
+  };
 
   render() {
     return (
       <div className="App" onClick={this.handleIncrement}>
-        <span>
-          {this.countSec()}
-        </span>
+        <span>{this.countSec()}</span>
       </div>
     );
   }
 
   countSec() {
-    const {count} = this.state;
-    if (count > 0) {
-      return <h1>ZOO GUIDE</h1>
+    const { count } = this.state;
+    if (count === 0) {
+      return <img src="./img/lion.jpg" alt="lion" />;
+    } else if (count === 1) {
+      return <h1>ZOO GUIDE</h1>;
     } else {
-      return <img src='./img/lion.jpg' alt='lion'/>
+      return (
+        <div>
+          <h2>Siapa Nama Kamu?</h2>
+          <input type="text"></input>
+          <br></br>
+          <a href="main" class="button">
+            Lanjut
+          </a>
+        </div>
+      );
     }
   }
 }
---
+++ zoo-guide/src/Navbar.js
@@ -0,0 +1,14 @@
+import { Component } from "react";
+
+class Navbar extends Component {
+  state = {};
+  render() {
+    return (
+      <nav className="nav">
+        <h1>Selamat Datang</h1>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
